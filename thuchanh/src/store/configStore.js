33955e1aec7b137a82040e481678854944b56910
hieuvnautotech <hieuvnautotech@gmail.com>
2023-09-12 11:12:27 +0700

    ok

--- thuchanh/src/store/configStore.js
+++ thuchanh/src/store/configStore.js
@@ -4,13 +4,15 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import createSagaMiddleware from 'redux-saga'
 import {initSagas} from '../sagas/index'
 
+const sagaMiddleware = createSagaMiddleware()
+const middlewares = [sagaMiddleware]
 const configureStore = () =>{
     const store = createStore(
         combineReducers({
             entries: entriesReducer,
 
-        })
-        combineWithDevTools((applyMiddleware(...middlewares)))
+        }),
+        composeWithDevTools((applyMiddleware(...middlewares)))
     )
     initSagas(sagaMiddleware)
     return store
