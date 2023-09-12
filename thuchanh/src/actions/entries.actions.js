24f57e11e654499c3532bdc84b2eb05960865582
hieuvnautotech <hieuvnautotech@gmail.com>
2023-09-12 13:51:57 +0700

    ok

--- thuchanh/src/actions/entries.actions.js
+++ thuchanh/src/actions/entries.actions.js
@@ -4,13 +4,13 @@ const types = {
 
 }
 export default types
-
+//get về để saga lắng nghe xong put populateEntries ra UI mới xem được
 export const getAllEntries = () => {
     return{
         type: types.GET_ENTRIES,
     }
 }
-
+//put populateEntries ra UI
 export const populateEntries = (entries) => {
     return { type: types.POPULATE_ENTRIES, payload: entries };
   };
